6e7cfcd50570ee0d2dc37736b0632d74bd8a8e84
Cristina Silva <crissilva.eng@gmail.com>
2021-06-16 20:26:45 -0300

    feat: 📈 adds healthcheck endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
+import { HealthModule } from './health/health.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
