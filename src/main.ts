2a798805ea7fda0043c591edbb23fdedbeb11476
Cristina Silva <crissilva.eng@gmail.com>
2021-06-16 23:41:44 -0300

    docs: 📝 adds swagger api doc in /api endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import Case from 'case';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(compression());
+  app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle(Case.title(process.env.npm_package_name))
+    .setDescription(`${process.env.npm_package_description} (rev: ${process.env.GIT_REV})`)
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
